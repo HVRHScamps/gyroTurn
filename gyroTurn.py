d991af695b39156222860e0a74429abe8a3362c8
Rivers <rivers.joseph@icloud.com>
2022-06-23 09:08:17 -0400

    Wrote gyro program

--- gyroTurn.py
+++ gyroTurn.py
@@ -2,4 +2,15 @@ import libhousy
 #You can define helper functions here, make sure to but them *above* the main function
 def main(robot: libhousy.robot):
     #Here is where your recurring code will go
-    print("Hello World!")
+    if robot.sense_hat.get_yaw() > -90:
+        robot.lDrive.Set(-0.5)
+        robot.rDrive.Set(0.5)
+        # stuff and things
+    elif robot.sense_hat.get_yaw() < -90: #overshot
+        robot.rDrive.Set(-0.5)
+        robot.lDrive.Set(0.5)
+
+    else:   
+        robot.rDrive.Set(0)
+        robot.lDrive.Set(0)
+    
