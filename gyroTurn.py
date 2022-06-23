24e2db81d47ed0432b9a5ed26378e320b7d09966
Braian-perez <bperez527@hotmail.com>
2022-06-23 09:23:16 -0400

    widen tolerance

--- gyroTurn.py
+++ gyroTurn.py
@@ -2,11 +2,11 @@ import libhousy
 #You can define helper functions here, make sure to but them *above* the main function
 def main(robot: libhousy.robot):
     #Here is where your recurring code will go
-    if robot.sense_hat.get_yaw() > -90:
+    if robot.sense_hat.get_yaw() > -88:
         robot.lDrive.Set(-0.5)
         robot.rDrive.Set(0.5)
         # stuff and things
-    elif robot.sense_hat.get_yaw() < -90: #overshot
+    elif robot.sense_hat.get_yaw() < -92: #overshot
         robot.rDrive.Set(-0.5)
         robot.lDrive.Set(0.5)
 
