e37946f374161ebc90338de4c16d655f420baee0
Braian-perez <bperez527@hotmail.com>
2022-06-24 09:32:29 -0400

    included proportional

--- gyroTurn.py
+++ gyroTurn.py
@@ -1,14 +1,19 @@
 import libhousy
 #You can define helper functions here, make sure to but them *above* the main function
+p=0.01
 def main(robot: libhousy.robot):
     #Here is where your recurring code will go
-    if robot.sense_hat.get_yaw() > -88:
-        robot.lDrive.Set(-0.5)
-        robot.rDrive.Set(0.5)
+    if robot.sense_hat.get_yaw() > -90:
+        error=robot.sense_hat.get_yaw()+90
+        speed= error *p
+        robot.lDrive.Set(-speed)
+        robot.rDrive.Set(speed)
         # stuff and things
-    elif robot.sense_hat.get_yaw() < -92: #overshot
-        robot.rDrive.Set(-0.5)
-        robot.lDrive.Set(0.5)
+    elif robot.sense_hat.get_yaw() < -90: #overshot
+        error=-90-robot.sense_hat.get_yaw()
+        speed= error *p
+        robot.rDrive.Set(-speed)
+        robot.lDrive.Set(speed)
 
     else:   
         robot.rDrive.Set(0)
