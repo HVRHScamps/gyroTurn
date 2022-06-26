aebe14be29e3a0eb46e8b0172d00d0a4ba37db6e
cannotilever <51347140+cannotilever@users.noreply.github.com>
2022-06-26 11:00:20 -0400

    Update gyroTurn.py

--- gyroTurn.py
+++ gyroTurn.py
@@ -1,21 +1,9 @@
+# inclue code to help us talk to the robot
 import libhousy
-#You can define helper functions here, make sure to but them *above* the main function
-p=0.01
-def main(robot: libhousy.robot):
-    #Here is where your recurring code will go
-    if robot.sense_hat.get_yaw() > -90:
-        error=robot.sense_hat.get_yaw()+90
-        speed= error *p
-        robot.lDrive.Set(-speed)
-        robot.rDrive.Set(speed)
-        # stuff and things
-    elif robot.sense_hat.get_yaw() < -90: #overshot
-        error=-90-robot.sense_hat.get_yaw()
-        speed= error *p
-        robot.rDrive.Set(-speed)
-        robot.lDrive.Set(speed)
 
-    else:   
-        robot.rDrive.Set(0)
-        robot.lDrive.Set(0)
+def main(robot: libhousy.robot):
+    # Here is where your recurring code will go
+    print("Hello World!")
     
+    # After everything is done, we tell the main program to stop us
+    return libhousy.
