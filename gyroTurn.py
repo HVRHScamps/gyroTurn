d119e6ec21feddc5be88410f7af45ee8057e3508
Lily <24danforthgld@cornwallschool.org>
2022-06-29 11:33:12 -0400

    stuff

--- gyroTurn.py
+++ gyroTurn.py
@@ -3,7 +3,11 @@ import libhousy
 
 def main(robot: libhousy.robot):
     # Here is where your recurring code will go
-    print("Hello World!")
-    
+    if robot.sense_hat.get_yaw() > -90:
+        robot.lDrive.Set(-0.4)
+        robot.rDrive.Set(0.4)
+    else:
+        robot.lDrive.Set(0)
+        robot.rDrive.Set(0)
+        return libhousy.DONE
     # After everything is done, we tell the main program to stop us
-    return libhousy.DONE
